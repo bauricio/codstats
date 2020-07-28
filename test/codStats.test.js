70fccc36518c3eeb81b7cbd58a225374aa03423c
Mauricio Sanches <mauricio.silveira.sanches@gmail.com>
2020-07-28 18:42:11 -0300

    Adds more tests and cleanup some code.

--- test/codStats.test.js
+++ test/codStats.test.js
@@ -17,9 +17,25 @@ describe('call of duty stats', () => {
     expect(stats['2020-07-17'].totalDeaths).toBe(3);
   });
 
-  test('calculates the kill/death ratio rounding', () => {
+  test('calculates the kill/death ratio rating', () => {
     expect(stats['2020-07-15'].kdRatio).toBe(1.44);
     expect(stats['2020-07-16'].kdRatio).toBe(1.53);
     expect(stats['2020-07-17'].kdRatio).toBe(3.33);
+  });
+
+  test('calculates the number of matches of a given day', () => {
+    expect(stats['2020-07-15'].numberOfMatches).toBe(9);
+    expect(stats['2020-07-16'].numberOfMatches).toBe(9);
+    expect(stats['2020-07-17'].numberOfMatches).toBe(2);
+  });
+
+  test('extract game information', () => {
+    const game = stats['2020-07-17'].matches[0];
+    expect(game).toEqual({
+      date: '2020-07-17T00:40:50-03:00',
+      kills: 2,
+      deaths: 1,
+      kdRatio: 2,
+    });
   });
 });
